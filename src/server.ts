868c4e7f69d6926e3023d9b2d9917411d13190d8
Dayane B X Gorgonha <dayanebismark18@gmail.com>
2022-05-04 17:21:39 -0300

    feat: created sending email

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,19 @@
 import express from 'express';
+import nodemailer from 'nodemailer';
 import { prisma } from './prisma';
 
 const app = express();
 
 app.use(express.json());
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "7db445cf8ddf36",
+    pass: "19f7402378c92e"
+  }
+});
 
 app.post('/feedbacks', async (req, res) => {
   const { type, comment, screenshot } = req.body;
@@ -14,6 +24,17 @@ app.post('/feedbacks', async (req, res) => {
       comment,
       screenshot,
     }
+  })
+
+  await transport.sendMail({
+    from: 'Equipe Feedget <equipe@feedget.com>',
+    to: 'Dayane Gorgonha <dayanebismark18@gmail.com>',
+    subject: 'Novo feedback',
+    html: [
+      `<div style="font-family: sans-serif; font-size: 16px; color: #111";>`,
+      `<p>Tipo do feedback: ${type}</p>`,
+      `<p>Comentario: ${comment}</p>`,
+    ].join('\n')
   })
 
   return res.status(201).json({ data: feedback });
